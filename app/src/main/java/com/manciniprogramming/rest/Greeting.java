8688bb55db93828c610c720d95fa9ffce6d66d91
frankmancininb <frank@bellabacci.com>
2016-12-09 19:06:22 -0800

    second commit

---
+++ app/src/main/java/com/manciniprogramming/rest/Greeting.java
@@ -0,0 +1,20 @@
+package com.manciniprogramming.rest;
+
+/**
+ * Created by user on 12/9/2016.
+ */
+
+public class Greeting {
+
+    private String id;
+    private String content;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+}
